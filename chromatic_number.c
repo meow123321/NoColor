7db67f89dc128931f7c63d373797d2f680c1674c
unknown <cis_nikoll23@yahoo.com>
2018-06-02 04:35:26 +0300

    04:35AM

--- chromatic_number.c
+++ chromatic_number.c
@@ -3,24 +3,34 @@
 #include <time.h>
 #include "Header.h"
 
-int chromatic_number(int **adjacency_matrix, int no_vertices){
+int color_vect[10];
 
+void  chromatic_number(int k){
+    int **adjacency_matrix;
     int line;
     int column;
-    int color;
+    int color=1;
 
-       color = 1;
+   // color_vect[k]=malloc(sizeof(int));
+    color_vect[k]=1;
+    for(line = 0; line < k; line++){
+        if(adjacency_matrix[line][k]==1 && color_vect[k]!= adjacency_matrix[line]){
+          //  color_vect[k]=malloc(sizeof(int));
+            color_vect[k]=adjacency_matrix[line]+1;
+        }
+    }
 
-    for (line = 0; line < no_vertices-1; line++){
+
+  /*  for (line = 0; line < no_vertices-1; line++){
+        k=line;
         for (column = line; column < no_vertices-1; column++){
-
-            if (adjacency_matrix[line][column] == 1 && adjacency_matrix[line+1][column] == 1){
+            if (adjacency_matrix[line][k+1] == 1 && adjacency_matrix[k+1][column] == 1){
                 printf("\n%d, %d", line, column);
                 color++;
             }
         }
     }
 
-    return color;
-
+    return color_vect;
+*/
 }
--- main.c
+++ main.c
@@ -3,43 +3,19 @@
 #include <time.h>
 #include "Header.h"
 
-int ** generator_matrix(int no_vertices){
+int color_vect[10];
+
+void  chromatic_number(int k){
     int **adjacency_matrix;
-    int line;
-    int column;
-
-    adjacency_matrix = malloc(no_vertices * sizeof(int*));
-    for (line = 0; line < no_vertices; line++){
-        adjacency_matrix[line] = malloc(no_vertices * sizeof(int));
-    }
-    //srand(time(0));
-    for (line = 0; line < no_vertices; line++){
-        for (column = line; column < no_vertices; column++){
-            if (line == column){
-               adjacency_matrix[line][column] = 0;
-            }else{
-                adjacency_matrix[line][column] = rand() % 2;
-                adjacency_matrix[column][line] = adjacency_matrix[line][column];
-            }
-            adjacency_matrix[line][column] = adjacency_matrix[column][line];
-        }
-    }
-
-    return adjacency_matrix;
-
-}
-
-int chromatic_number(int **adjacency_matrix, int k){
-    int * color_vect;
     int line;
     int column;
     int color=1;
 
-    color_vect[k]=malloc(sizeof(int));
+   // color_vect[k]=malloc(sizeof(int));
     color_vect[k]=1;
     for(line = 0; line < k; line++){
         if(adjacency_matrix[line][k]==1 && color_vect[k]!= adjacency_matrix[line]){
-            color_vect[k]=malloc(sizeof(int));
+          //  color_vect[k]=malloc(sizeof(int));
             color_vect[k]=adjacency_matrix[line]+1;
         }
     }
@@ -53,10 +29,10 @@ int chromatic_number(int **adjacency_matrix, int k){
                 color++;
             }
         }
-    }*/
+    }
 
     return color_vect;
-
+*/
 }
 
 int main(){
@@ -65,7 +41,6 @@ int main(){
     int line;
     int column;
     int color;
-    int * color_vect;
 
     adjacency_matrix = malloc(no_vertices * sizeof(int*));
     for (line = 0; line < no_vertices; line++){
@@ -89,7 +64,7 @@ int main(){
     */
 
     for(line=0; line < no_vertices; line++){
-        color_vect=chromatic_number(adjacency_matrix, no_vertices);
+        chromatic_number(line);
    }
 
    for(line=0; line<no_vertices; line++){
